753c70aba5afced6c4407b32fe57bbbecbb546f3
ganesh <ganeshsadhashivam@outlook.com>
2023-05-27 23:33:45 +0530

    design completed

---
+++ RockPaperScissor/script.js
@@ -0,0 +1,30 @@
+const rock = document.getElementById("rock-btn");
+const paper = document.getElementById("paper-btn");
+const scissor = document.getElementById("scissor-btn");
+
+console.log(rock.innerText);
+//lines
+
+const lineOne = document.getElementById("line-box-1");
+const lineTwo = document.getElementById("line-box-2");
+const lineThree = document.getElementById("line-box-3");
+function player() {
+  lineOne.style.display = "none";
+  lineTwo.style.display = "none";
+  lineThree.style.display = "none";
+  console.log(e.target.value);
+  const char = false;
+  while (false) {
+    switch (char) {
+    }
+  }
+}
+
+function computer() {}
+rock.addEventListener(
+  onclick,
+  function (e) {
+    alert(e.target.tagName);
+  },
+  false
+);
